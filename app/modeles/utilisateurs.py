e6c08ba2eb7c3a499312a5f4093e147987be234b
Maxime Challon <maxime.challon@gmail.com>
2020-03-01 11:51:10 +0100

    commentaire des modeles utilisateurs

--- app/modeles/utilisateurs.py
+++ app/modeles/utilisateurs.py
@@ -4,29 +4,56 @@ from wtforms.validators import DataRequired, Email, EqualTo
 from app.modeles.donnees import User
 from flask_pagedown.fields import PageDownField
 
+# création de la classe du formulaire pour la connexion
 class LoginForm(FlaskForm):
     user_name = StringField('Nom d\'utilisateur', validators=[DataRequired()])
     user_password = PasswordField('Mot de passe', validators=[DataRequired()])
     remember_me = BooleanField('Se souvenir de moi')
+
     submit = SubmitField('Connexion')
 
+
+# création de la classe du formulaire pour l'inscription
 class RegistrationForm(FlaskForm):
     username = StringField('Nom d\'utilisateur', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Mot de passe', validators=[DataRequired()])
+    # vérification que le mot de passe fourni la deuxième fois est le même que celui fourni la première fois
     password2 = PasswordField('Mot de passe', validators=[DataRequired(), EqualTo('password')])
+
     submit = SubmitField('S\'inscrire')
 
+    # vérification que le nom d'utilisateur n'existe pas
     def validate_username(self, username):
+        """
+        Permet de vérifier que le nom d'utilisateur donné n'existe pas déjà
+        :param username: nom de l'utilisateur
+        :type username: str
+        :return: None
+        :rtype: None
+        """
+        # récupération du premier utilisateur dont le nom est le même que celui donné en paramètre
         user = User.query.filter_by(user_name=username.data).first()
+        # s'il y a un retour d'un utilisateur, alors une erreur s'affiche
         if user is not None:
             raise ValidationError('Nom d\'utilisateur déjà enregistré')
 
     def validate_email(self, email):
-        user = User.query.filter_by(user_mail=email.data).first()
-        if user is not None:
+        """
+        Permet de vérifier que le mail fourni n'existe pas déjà
+        :param email: adresse email de l'utilisateur
+        :type email: str
+        :return: None
+        :rtype: None
+        """
+        # récupération du premier mail identique à celui fourni en paramètre
+        mail = User.query.filter_by(user_mail=email.data).first()
+        # s'il y a un retour d'une adresse mail, alors une erreur s'affiche
+        if mail is not None:
             raise ValidationError('Adresse email déjà enregistrée')
 
+
+# création de la classe du formulaire pour l'édition du profil de l'utilisateur
 class EditProfileForm(FlaskForm):
     user_name = StringField('Nom d\'utilisateur', validators=[DataRequired()])
     user_mail = StringField('Email', validators=[DataRequired(), Email()])
@@ -37,17 +64,26 @@ class EditProfileForm(FlaskForm):
     user_birthyear = StringField('Année de naissance', validators=[DataRequired()])
     user_promotion_date = StringField('Promotion (année du diplôme)')
     user_description = TextAreaField('Description')
+
     submit = SubmitField('Mettre à jour')
 
+
+# création de la classe du formulaire pour publier un post
 class PostForm(FlaskForm):
     titre = StringField('Titre de la discussion', validators=[DataRequired()])
     message = PageDownField("Ecrivez ici votre message (MarkDown possible)", validators=[DataRequired()])
+
     submit = SubmitField('Envoyer')
 
+
+# création de la classe du formulaire pour l'écriture d'un commentaire
 class CommentForm(FlaskForm):
     message = PageDownField("Votre commentaire", validators=[DataRequired()])
+
     submit = SubmitField('Commenter')
 
+
+# création de la classe du formulaire pour l'édition d'une expérience professionnelle
 class CVForm(FlaskForm):
     cv_nom_poste = StringField('Dénomination du poste', validators=[DataRequired()])
     cv_nom_employeur = StringField("Nom de l'entreprise ou du service", validators=[DataRequired()])
@@ -55,4 +91,5 @@ class CVForm(FlaskForm):
     cv_annee_debut = StringField("Année de début d'exercice", validators=[DataRequired()])
     cv_annee_fin = StringField("Année de fin d'exercice", validators=[DataRequired()])
     cv_description_poste = TextAreaField('Description des missions du poste')
+
     submit = SubmitField('Enregistrer')

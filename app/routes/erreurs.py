5cb30d8bb25e40e7c6fa403e46606aaf8d0fd09a
Maxime Challon <maxime.challon@gmail.com>
2020-03-13 14:45:11 +0100

    gestion de l'erreur 401

--- app/routes/erreurs.py
+++ app/routes/erreurs.py
@@ -1,9 +1,16 @@
 from flask import render_template
-from ..app import app, db
+from ..app import app
+
+
+@app.errorhandler(401)
+def not_found_error(error):
+    return render_template('erreurs/401.html'), 401
+
 
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('erreurs/404.html'), 404
+
 
 @app.errorhandler(502)
 def internal_error(error):

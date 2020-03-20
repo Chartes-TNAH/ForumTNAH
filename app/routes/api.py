ffa87bc2c39e7189186d20d21062c5d70a52b98e
Maxime Challon <maxime.challon@gmail.com>
2020-03-20 11:57:16 +0100

    création de la route api_utilisateurs_browse

--- app/routes/api.py
+++ app/routes/api.py
@@ -3,8 +3,13 @@ from urllib.parse import urlencode
 
 from ..app import app
 from ..constantes import RESULTATS_PAR_PAGE, API_ROUTE
-from ..modeles.donnees import Post
+from ..modeles.donnees import Post, User
 
+# routes dans l'ordre
+# /api/posts/<int:post_id>
+# /api/posts
+# /api/utilisateurs
+# /api/utilisateurs/<int:user_id>
 
 def Json_404():
     response = jsonify({"erreur": "Unable to perform the query"})
@@ -84,6 +89,70 @@ def api_posts_browse():
         if motclef:
             arguments["q"] = motclef
         dict_resultats["links"]["prev"] = url_for("api_posts_browse", _external=True)+"?"+urlencode(arguments)
+
+    response = jsonify(dict_resultats)
+    response.headers['Content-Type'] = 'application/vnd.api+json'
+    return response
+
+
+@app.route(API_ROUTE+"/utilisateurs")
+def api_utilisateurs_browse():
+    """ Route permettant le retour des données des utilisateurs
+    On s'inspirera de http://jsonapi.org/ faute de pouvoir trouver temps d'y coller à 100%
+    """
+    # q est très souvent utilisé pour indiquer une capacité de recherche
+    motclef = request.args.get("q", None)
+    page = request.args.get("page", 1)
+
+    if isinstance(page, str) and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
+    if motclef:
+        query = User.query.filter(
+            User.user_name.like("%{}%".format(motclef))
+        )
+    else:
+        query = User.query
+
+    try:
+        resultats = query.paginate(page=page, per_page=RESULTATS_PAR_PAGE)
+    except Exception:
+        return Json_404()
+
+    dict_resultats = {
+        "jsonapi": {
+            "version": "1.0"
+        },
+        "links": {
+            "self": request.url
+        },
+        "data": [
+            user.user_to_json()
+            for user in resultats.items
+        ],
+        "meta": {
+            "copyright": "2020 - Ecole nationale des Chartes",
+            "results": resultats.total
+        }
+    }
+
+    if resultats.has_next:
+        arguments = {
+            "page": resultats.next_num
+        }
+        if motclef:
+            arguments["q"] = motclef
+        dict_resultats["links"]["next"] = url_for("api_utilisateurs_browse", _external=True)+"?"+urlencode(arguments)
+
+    if resultats.has_prev:
+        arguments = {
+            "page": resultats.prev_num
+        }
+        if motclef:
+            arguments["q"] = motclef
+        dict_resultats["links"]["prev"] = url_for("api_utilisateurs_browse", _external=True)+"?"+urlencode(arguments)
 
     response = jsonify(dict_resultats)
     response.headers['Content-Type'] = 'application/vnd.api+json'

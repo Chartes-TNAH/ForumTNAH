8d7afaaaa13403a094a701e7817e9ad0e31f5148
Maxime Challon <maxime.challon@gmail.com>
2020-03-21 14:35:53 +0100

    modification de l'image par défaut des compétences et des lieux

--- app/routes/generales.py
+++ app/routes/generales.py
@@ -202,10 +202,13 @@ def competences():
     dictionnaire_distinct = {}
 
     for competence in competences:
-        # récupération de l'image
-        image = get_first_image(competence.competence_label)
-        # remplissage du dictionnaire
-        dictionnaire_distinct[competence.competence_label] = image
+        try:
+            # récupération de l'image
+            image = get_first_image(competence.competence_label)
+            # remplissage du dictionnaire
+            dictionnaire_distinct[competence.competence_label] = image
+        except:
+            dictionnaire_distinct[competence.competence_label] = "https://thenounproject.com/term/skills/369287/"
 
     # comptage du nombre de nombre de mots-clés
     compteur_competences = len(dictionnaire_distinct)
@@ -272,11 +275,14 @@ def lieux():
     dictionnaire_distinct = {}
 
     for experience in experiences:
-        # récupération de l'image
-        image = get_first_image(experience.cv_ville)
-        # remplissage du dictionnaire
-        dictionnaire_distinct[experience.cv_ville] = image
-
+        try:
+            # récupération de l'image
+            image = get_first_image(experience.cv_ville)
+            # remplissage du dictionnaire
+            dictionnaire_distinct[experience.cv_ville] = image
+        except:
+            dictionnaire_distinct[experience.cv_ville] = "http://www.istockphoto.com/vector/city-icons-set-signs-and-symbols-gm497023616-78886163"
+    print(dictionnaire_distinct)
     # comptage du nombre de nombre de mots-clés
     compteur_lieux = len(dictionnaire_distinct)
 

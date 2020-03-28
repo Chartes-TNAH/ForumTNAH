18950d0ef66a245dfbcc4bfd6865c9e2b0724dd3
Maxime Challon <maxime.challon@gmail.com>
2020-03-28 11:09:22 +0100

    raccourcissement de la phase d'insertion de données

--- run.py
+++ run.py
@@ -1,26 +1,17 @@
 from app.app import app, db
-from app.constantes import DEBUG
-from app.sql_initilisation import users, posts, comments, cvs, competences, skills, messages, followers
+from app.constantes import DEBUG, SQL_INIT
+from tqdm import tqdm
+import os
 
 if __name__ == "__main__":
-    print("Initialisation de la base de données en cours...")
+    print("Initialisation de la base de données en cours")
     db.drop_all()
-    print("Création des tables de la base de données...")
+    print("Création des tables de la base de données")
     db.create_all()
-    print("Insertion de données utilisateurs tests...")
-    db.engine.execute(users)
-    print("Insertion de posts tests...")
-    db.engine.execute(posts)
-    print("Insertion de commentaires tests...")
-    db.engine.execute(comments)
-    print("Insertion de expériences utilisateurs tests...")
-    db.engine.execute(cvs)
-    print("Insertion de compétences tests...")
-    db.engine.execute(competences)
-    print("Insertion de compétences utilisateurs tests...")
-    db.engine.execute(skills)
-    print("Insertion de messages personnels tests...")
-    db.engine.execute(messages)
-    print("Insertion de suivis entre utilisateurs tests...")
-    db.engine.execute(followers)
+    # insertion des données d'exemple
+    for fichier in tqdm(os.listdir(SQL_INIT)):
+        with open(SQL_INIT + fichier, 'r') as f:
+            print("Insertion de données d'exemple dans la table  " + fichier.replace('.sql', ''))
+            for line in tqdm(f):
+                db.engine.execute(line)
     app.run(debug=DEBUG)
